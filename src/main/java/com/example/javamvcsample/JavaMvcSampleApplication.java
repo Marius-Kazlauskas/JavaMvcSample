8108fa6de6a8815e89eeb2f75a770dec2675271a
DESKTOP-BDIUN87\Marius <mar.kazlauskas@gmail.com>
2024-03-10 12:40:41 +0200

    init

---
+++ src/main/java/com/example/javamvcsample/JavaMvcSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamvcsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMvcSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMvcSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javamvcsample/JavaMvcSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamvcsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMvcSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
