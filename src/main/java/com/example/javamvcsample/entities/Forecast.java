f74e0733aaf2fdadc9ce2861c04d0c4053f96309
DESKTOP-BDIUN87\Marius <mar.kazlauskas@gmail.com>
2024-03-11 17:43:50 +0200

    Added DB Table

---
+++ src/main/java/com/example/javamvcsample/entities/Forecast.java
@@ -0,0 +1,22 @@
+// https://dev.mysql.com/downloads/mysql/
+// https://www.mysql.com/products/workbench/
+// Naudojau mysql server 8.0 versiją, nes buvo niuansas, kad workbench su 8.3 (naujausia) neveikė
+// Sukurti nauja forecastdb ir paleisti programą - turėtų forecastdb automtiškai atsirasti nauje lentelė Forecast
+
+package com.example.javamvcsample.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Forecast {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public Integer id;
+    public String date;
+    public String temperature;
+    public String city;
+    public int userId;
+}
