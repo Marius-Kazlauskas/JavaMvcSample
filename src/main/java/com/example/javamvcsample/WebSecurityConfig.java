f6408f9f06ff789a6479c37f82e5fef45f846d31
DESKTOP-BDIUN87\Marius <mar.kazlauskas@gmail.com>
2024-03-11 21:32:49 +0200

    Enable login

---
+++ src/main/java/com/example/javamvcsample/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+//Projekto kūrimo metu, Spring lange pasirenkam Security -> Spring Security ir ten yra linkas
+//https://spring.io/guides/gs/securing-web
+package com.example.javamvcsample;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        //.requestMatchers("/public").permitAll() //leidžiami puslapiai be prisijungimo
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        //.loginPage("/login") //jei norim naudoti default login puslapį šitą uždiseiblinam
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
